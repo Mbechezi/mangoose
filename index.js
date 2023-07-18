ae2b2d39d92ba34a37e17e0cb4cee3930e570251
Mbechezi <117647946+Mbechezi@users.noreply.github.com>
2023-07-18 15:30:05 +0000

    continuation

--- index.js
+++ index.js
@@ -35,24 +35,24 @@ p.save();
 Person.create([
     {
         Name: 'Raznati Radjabou',
-        Age:29,
-        favoriteFoods :['Manioc', 'samboussa']
+        Age: 29,
+        favoriteFoods: ['Manioc', 'samboussa']
     },
-    
+
     {
         Name: 'Zayane Hassane',
-        Age:2,
-        favoriteFoods :['Lait', 'céréales']
+        Age: 2,
+        favoriteFoods: ['Lait', 'céréales']
     },
     {
         Name: 'Namir Mistoihi',
-        Age:3,
-        favoriteFoods :['Jus', 'Burger']
+        Age: 3,
+        favoriteFoods: ['Jus', 'Burger']
     },
     {
         Name: 'Milane Mounir',
-        Age:1,
-        favoriteFoods :['Fruits', 'légumes']
+        Age: 1,
+        favoriteFoods: ['Fruits', 'légumes']
     },
 ]
 );
@@ -61,11 +61,30 @@ Person.create([
 
 
 //Use model.findOne() to Return a Single Matching Document from the Database
-Person.findOne({favoriteFoods:'samboussa'})
+Person.findOne({ favoriteFoods: 'samboussa' })
 
 
 //Use model.findById() to Search Your Database By _id
-Person.findById({}) 
+Person.findById("64b57d154763023a9b8342c9")
 
 //Updates by Running Find, Edit, then Save
 
+Person.findById("64b57d154763023a9b8342c9").then((personId) => {
+    personId.favoriteFoods.push('Hamburger'); personId.save();
+})
+
+//New Updates on a Document Using model.findOneAndUpdate()
+Person.findOneAndUpdate(
+    { Name:"Mouslim Youssouf" },
+    { age: 20 },
+    { new: true }
+)
+
+
+//Delete One Document Using model.findByIdAndRemove
+Person.findByIdAndRemove("64b57d154763023a9b8342cc")
+
+//MongoDB and Mongoose - Delete Many Documents with model.remove()
+
+
+//Chain Search Query Helpers to Narrow Search Results
